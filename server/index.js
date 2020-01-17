dae79626f636b4474d6f79d24f69ab311f0ae84d
kunwalle <khanglqse@gmail.com>
2020-01-17 22:58:38 +0700

    add host

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send({ok: true})
-});
-
+var allowedOrigins = ['https://api.khang.test',
+                      'http://yourapp.com'];
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+app.get('/', function (req, res, next) {
+  res.json({ok: true})
+})
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!')
 });
