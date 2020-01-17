0179665444c3a0686f5204be2ea5fe7937246c76
kunwalle <khanglqse@gmail.com>
2020-01-17 22:58:38 +0700

    add host

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send({ok: true})
-});
+var whitelist = ['https://dashboard.khang.test', 'https://khang.test']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
+app.get('/', cors(corsOptions), function (req, res, next) {
+  res.json({ok: true})
+})
 app.listen(5000, () => {
   console.log('Example app listening on port 5000!')
 });
